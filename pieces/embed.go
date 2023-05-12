a573a5cf0f866870635e4ca9f596bfd303633b31
FilipMyhren <filip.myhren@ingka.ikea.com>
2023-05-12 10:09:45 +0200

    Added images of pieces

---
+++ pieces/embed.go
@@ -0,0 +1,44 @@
+package pieces
+
+import (
+	_ "embed"
+)
+
+var (
+	//go:embed white-pawn.png
+	Whitepawn_png []byte
+
+	//go:embed white-rook.png
+	Whiterook_png []byte
+
+	//go:embed white-bishop.png
+	Whitebishop_png []byte
+
+	//go:embed white-knight.png
+	Whiteknight_png []byte
+
+	//go:embed white-king.png
+	Whiteking_png []byte
+
+	//go:embed white-queen.png
+	Whitequeen_png []byte
+
+	//go:embed black-pawn.png
+	Blackpawn_png []byte
+
+	//go:embed black-rook.png
+	Blackrook_png []byte
+
+	//go:embed black-bishop.png
+	Blackbishop_png []byte
+
+	//go:embed black-knight.png
+	Blackknight_png []byte
+
+	//go:embed black-king.png
+	Blackking_png []byte
+
+	//go:embed black-queen.png
+	Blackqueen_png []byte
+
+)
